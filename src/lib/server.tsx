f0ec33720d4b980d9c715e9c573bc44de73e633f
Alex Henderson <alex@alexhenderson.me>
2019-05-11 11:25:54 -0700

    feat: add server

---
+++ src/lib/server.tsx
@@ -0,0 +1,110 @@
+import path from 'path';
+import {Server} from 'http';
+import express, {Express} from 'express';
+import chalk from 'chalk';
+
+import renderApp, {GetAppFn} from '../middlewares/render-app';
+import HTML, {HTMLProps} from '../components/html';
+
+export type IsoServerOpts = {
+  html?: (HTMLProps) => JSX.Element;
+  port?: number;
+  debug?: boolean;
+};
+
+class IsoServer {
+  defaultOpts: IsoServerOpts = {
+    html: HTML,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+    debug: false,
+  };
+  opts: IsoServerOpts;
+  getAppFn: GetAppFn;
+  middlewares: any[];
+  app?: Express;
+  server?: Server;
+
+  constructor(getApp: GetAppFn, opts: IsoServerOpts = {}) {
+    if (!getApp) {
+      throw new Error('Must have an app selector');
+    }
+
+    this._listenCallback = this._listenCallback.bind(this);
+    this._log = this._log.bind(this);
+    this._registerMiddleware = this._registerMiddleware.bind(this);
+    this._registerCommonMiddleware = this._registerCommonMiddleware.bind(this);
+
+    this.opts = {
+      ...this.defaultOpts,
+      ...opts,
+    };
+    this.getAppFn = getApp;
+    this.middlewares = [];
+  }
+
+  use(...args) {
+    this.middlewares.push([...args]);
+  }
+
+  start() {
+    this.app = express();
+
+    this.middlewares.forEach(this._registerMiddleware);
+
+    this._registerCommonMiddleware();
+
+    this.server = this.app.listen(this.opts.port, this._listenCallback);
+
+    return this.server;
+  }
+
+  _registerMiddleware(middleware) {
+    if (this.app) {
+      this._debug(`Registering middleware`, middleware);
+      this.app.use(...middleware);
+    }
+  }
+
+  _registerCommonMiddleware() {
+    if (this.app) {
+      this._debug('Registering common middlewares');
+      this.app.use(this._log);
+      this.app.use('/static', express.static(path.join(__dirname, '/static')));
+      this.app.use(express.static(path.join(__dirname, 'public')));
+      this.app.use(renderApp(this.getAppFn, this.opts.html));
+    }
+  }
+
+  _log(req, res, next) {
+    let deferred = [];
+    const actualLog = console.log;
+    const actualError = console.error;
+    const defer = fn => {
+      deferred.push(fn);
+    };
+    console.debug = (...args) => defer(this._debug.bind(this, ...args));
+    console.log = (...args) => defer(this._debug.bind(this, ...args));
+    console.error = (...args) => defer(this._debug.bind(this, ...args));
+
+    next();
+    console.log = actualLog;
+    console.error = actualError;
+    console.log(`${req.method} ${req.url} -> ${res.statusCode}`);
+    deferred.forEach(fn => fn());
+  }
+
+  _listenCallback() {
+    console.log(
+      chalk.green(`Isomorphic Server Listening on port ${this.opts.port}`)
+    );
+    this._debug(
+      `Server running with ${this.middlewares.length} custom middlewares`
+    );
+  }
+
+  _debug(...args) {
+    if (this.opts.debug) {
+      console.debug(chalk.gray(...args));
+    }
+  }
+}
